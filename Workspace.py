b82747aaa040d9f2af1a64986650460a0cb35611
Dilan <102928901+dilankoc@users.noreply.github.com>
2022-06-06 16:08:19 +0300

    Add files via upload
    First coding

---
+++ Workspace.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# this is my first line of code
+
+
+# In[2]:
+
+
+print("My name is Dilan")
+print("The date is 06.06.2022")
+
+
+# In[3]:
+
+
+# write a program to analyse real estate data
+
+
+# In[4]:
+
+
+# create variables
+
+
+# In[5]:
+
+
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+
+# In[6]:
+
+
+total_sales = a + b + c + d + e
+
+
+# In[7]:
+
+
+print(total_sales)
+
+
+# In[8]:
+
+
+# average sales 
+
+
+# In[9]:
+
+
+avg_sales = total_sales / 5
+
+
+# In[10]:
+
+
+print(avg_sales)
+
+
+# In[11]:
+
+
+# create a new variables
+
+
+# In[12]:
+
+
+property_price_C = (67000 - 6500) 
+
+
+# In[13]:
+
+
+print(property_price_C)
+
+
+# In[ ]:
+
+
+
+
